820fa2b0833e816dd339e17d027c5db1e745afc7
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-02-14 15:10:37 +0800

    edited introduction paragraph

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
         return (
             <div>
                 <h1>Heyyyy there!!!</h1>
-                <p>My name is Amelia and I am a former Journalist that is learning the tools of the trade to become a Software Developer.</p>
+                <p>My name is Amelia and I am a former Journalist who is learning the tools of the trade to become a Software Developer.</p>
                 <p>Feel free to contact me if you have any wild or funny stories at work to share!</p>
             </div>
         )
