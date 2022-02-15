f4a6e37f3942b971b24fc5ebb5aadabc5a1ff9cd
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-02-15 15:38:04 +0800

    linked Projects.js to App.js so that it can be rendered in the latter and appear on the lower half of the main page

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Chomp',
+        description: 'A mobile app that helps indecisive groups decide on where to eat!',
+        link: 'https://letschomp.herokuapp.com',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'ChompFood',
+        description: 'Food delivery app that satiates cravings within the comfort of your home!',
+        link: 'chompfood.herokuapp.com',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Snippets of my published articles',
+        description: 'A collection of some articles that I have written that were published on the likes of MOJO News, Esperanto Magazine and Singapore Turf Club.',
+        link: 'https://amelialimyh.wixsite.com/resume',
+        image: project3
+    }
+];
+
+export default PROJECTS;
